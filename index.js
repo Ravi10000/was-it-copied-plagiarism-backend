181651e9db3b0bb17f58ad8c8ea9ad703f0e03b1
ravisince2k <pabhinav400@gmail.com>
2023-05-19 16:50:08 +0530

    change base url message

--- index.js
+++ index.js
@@ -63,7 +63,8 @@ app.use("/api/webhooks", webhookRoues);
 
 // home route
 app.get("/", (req, res) => {
-  res.send("API Hosted On " + process.env.API_URL);
+  // res.send("API Hosted On " + process.env.API_URL);
+  res.send("plagiarism checker api");
 });
 
 app.listen(4000, () => {
